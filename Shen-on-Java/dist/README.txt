197e9499289eb93819f6b756bae6a5cec395eb26
taotree <joel@mentics.com>
2012-12-08 15:34:41 -0800

    license/readme stuff

---
+++ Shen-on-Java/dist/README.txt
@@ -0,0 +1,7 @@
+This port is subject to the Shen license. See LICENSE.txt
+
+Documentation is forming at:
+http://www.mentics.com/wk/shen/java/shen_on_java
+
+Source code is available at:
+https://www.assembla.com/code/shen-on-java/git/nodes
--- Shen-on-Java/dist/shen-on-java.bat
+++ Shen-on-Java/dist/shen-on-java.bat
@@ -1,4 +1,4 @@
 rem JDK_HOME must point to a JDK, not just a JRE
 rem set JDK_HOME=C:/dev/java/jdk1.7.0_06
 rem echo %JDK_HOME%
-"%JDK_HOME%/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;shen-on-java-0.6.0.jar com.mentics.shenj.REPL
+"%JDK_HOME%/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;shen-on-java-0.6.0.jar com.mentics.shenj.REPL %1
