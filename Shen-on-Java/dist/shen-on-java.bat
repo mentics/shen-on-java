1d816b3ad2324e309a188cdedc4149a0f4e52415
taotree <joel@mentics.com>
2012-12-18 01:29:40 -0800

    version 0.7.0

--- Shen-on-Java/dist/shen-on-java.bat
+++ Shen-on-Java/dist/shen-on-java.bat
@@ -1,4 +1,4 @@
 rem JDK_HOME must point to a JDK, not just a JRE
 rem set JDK_HOME=C:/dev/java/jdk1.7.0_06
 rem echo %JDK_HOME%
-"%JDK_HOME%/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;shen-on-java-0.6.1.jar com.mentics.shenj.REPL %1
+"%JDK_HOME%/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;shen-on-java-0.7.0.jar com.mentics.shenj.REPL %1
