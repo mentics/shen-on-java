ab6ebaf6d7cf2c354c68235a9b8ca4f79c4a1224
taotree <joel@mentics.com>
2013-04-06 20:01:00 -0700

    Version 0.8.0

--- Shen-on-Java/dist/shen-on-java.bat
+++ Shen-on-Java/dist/shen-on-java.bat
@@ -1,4 +1,1 @@
-rem JDK_HOME must point to a JDK, not just a JRE
-rem set JDK_HOME=C:/dev/java/jdk1.7.0_06
-rem echo %JDK_HOME%
-"%JDK_HOME%/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;shen-on-java.jar com.mentics.shenj.REPL %1
+java -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar;lib/ecj-4.2.1.jar;shen-on-java.jar com.mentics.shenj.REPL %1
--- Shen-on-Java/dist/shen-on-java.sh
+++ Shen-on-Java/dist/shen-on-java.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-# JDK_HOME=/usr/lib/jvm/java-7-oracle
-# export JDK_HOME
-
-"$JDK_HOME/bin/java" -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar:shen-on-java.jar com.mentics.shenj.REPL $1
+java -Xss128m -XX:MaxPermSize=512m -Xmx512m -classpath lib/kryo-debug-2.20-all.jar:lib/ecj-4.2.1.jar:shen-on-java.jar com.mentics.shenj.REPL $1
